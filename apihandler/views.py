894e5e779369f00202cccfcf2f5519664ef7590f
lucasmdpereira <lucasmdpereira@gmail.com>
2022-10-20 19:42:19 -0300

    :truck: Configuração de servidor e rota principal

---
+++ apihandler/views.py
@@ -0,0 +1,6 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>API Handler</h1>')
