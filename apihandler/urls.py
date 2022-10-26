8da1b7fc65c03fc6749624cb136cacb0c59197bc
lucasmdpereira <lucasmdpereira@gmail.com>
2022-10-26 10:13:33 -0300

    :sparkles: Front-end, e rotas com retorno em Json criados

--- apihandler/urls.py
+++ apihandler/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path, include
-from apihandler.views import all_data, websites, detail, users
+from apihandler.views import all_data, websites, detail, users, raw_data, raw_websites, raw_detail, raw_users
 
 urlpatterns = [
     path('', all_data),
@@ -8,6 +8,12 @@ urlpatterns = [
     path('users/detail', detail, name='detail'),
     path('users', users, name='users'),
     path('accounts/', include('django.contrib.auth.urls')),
+    
+    path('raw', raw_data, name='raw_data'),
+    path('raw_users/websites', raw_websites, name='raw_websites'),
+    path('raw_users/detail', raw_detail, name='raw_detail'),
+    path('raw_users', raw_users, name='raw_users'),
+    
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
--- apihandler/views.py
+++ apihandler/views.py
@@ -2,14 +2,14 @@ from django.http.response import HttpResponse
 from django.template import loader
 from src.handlers.dataHandler import DataAPI
 from django.contrib.auth.decorators import login_required 
-#import json
+import json
 
 
 @login_required
 def all_data(request):
     template = loader.get_template('apihandler/all_data.html')
     context = {
-        'data': DataAPI().list_users_by_query("")
+        'data': DataAPI().entries
     }
     return HttpResponse(template.render(context, request))
 
@@ -34,11 +34,39 @@ def users(request):
     # template = loader.get_template('apihandler/detail.html')
     query = ''
     if request.GET.getlist("name"):
-        query = request.GET.getlist("name")[0]
-    
-    print(query)
-    
+        query = request.GET.getlist("name")[0]  
     context = {
         'users': DataAPI().list_users_by_query(query),
     }
     return HttpResponse(context['users'])
+
+@login_required
+def raw_data(request):
+    context = {
+        'data': json.dumps(DataAPI().entries)
+    }
+    return HttpResponse(context['data'])
+
+@login_required
+def raw_websites(request):
+    context = {
+        'data': DataAPI().list_users_websites
+    }
+    return HttpResponse(context['data'])
+
+@login_required
+def raw_detail(request):
+    context = {
+        'data': DataAPI().list_users_name_email_company
+    }
+    return HttpResponse(context['data'])
+
+@login_required
+def raw_users(request):
+    query = ''
+    if request.GET.getlist("name"):
+        query = request.GET.getlist("name")[0]  
+    context = {
+        'data': DataAPI().list_users_by_query(query),
+    }
+    return HttpResponse(context['data'])
